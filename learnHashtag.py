8092912bf49ed427c268e66dbfcda5fc5d872435
Alex Pimania <randomiseme33@gmail.com>
2017-08-08 10:58:57 +1000

    Update learnHashtag.py

--- learnHashtag.py
+++ learnHashtag.py
@@ -1,10 +1,27 @@
 import tweepy
 
+
+def getConfig():
+   import json
+   config = json.loads(open("config.json").read())
+   return config
+
+
 def getHashtag():
   hashtag = input("Enter a hashtag to create a predictive database with: ")
   return hashtag.replace("#", "")
 
 
-def getRelevantTweetIDs()
+def getTweets()
   hashtag = getHashtag()
+  
+ 
+def getNgrams():
+  pass
+
+
+def savePredictionData():
+    pass
+  
+
   
