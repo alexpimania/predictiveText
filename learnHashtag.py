1e51b4f31ef4e4ba5a47e75957b8d7baeb3be63a
Alex Pimania <randomiseme33@gmail.com>
2017-08-08 11:05:58 +1000

    Update learnHashtag.py

--- learnHashtag.py
+++ learnHashtag.py
@@ -2,9 +2,9 @@ import tweepy
 
 
 def getConfig():
-   import json
-   config = json.loads(open("config.json").read())
-   return config
+  import json
+  config = json.loads(open("config.json").read())
+  return config
 
 
 def getHashtag():
@@ -12,16 +12,37 @@ def getHashtag():
   return hashtag.replace("#", "")
 
 
-def getTweets()
+def initTwitterApi():
+  config = getConfig()
+  import tweepy
+  twitterKeys = config["twitterKeys"]
+  auth = tweepy.OAuthHandler(twitterKeys[0], twitterKeys[1])
+  auth.set_access_token(twitterKeys[2], twitterKeys[3])
+  return tweepy.API(auth, wait_on_rate_limit_notify=True, wait_on_rate_limit=True)
+
+
+def getTweets():
+  import tweepy
+  import string
+  tweets = []
+  config = getConfig()
+  api = initTwitterApi()
   hashtag = getHashtag()
-  
+  tweetCount = config["tweetCount"]
+  search = tweepy.Cursor(api.search, q="#" + hashtag, tweet_mode="extended", lang="en")
+  for tweet in search.items(tweetCount):
+    tweetText = removeText(tweet._json["full_text"]).lower().strip()
+    tweetText = "".join([item for item in list(tweetText) if item not in list(string.punctuation + "#!()-$@/'?")])
+    tweets.append(tweetText)
+  return tweetText
+
  
 def getNgrams():
   pass
 
 
 def savePredictionData():
-    pass
+  pass
   
 
   
