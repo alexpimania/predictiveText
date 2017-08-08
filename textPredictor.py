73f44ce4ad256878c1e6fdda43ec7060a0e7ded2
Alex Pimania <randomiseme33@gmail.com>
2017-08-08 17:00:15 +1000

    Update textPredictor.py

--- textPredictor.py
+++ textPredictor.py
@@ -1,7 +1,14 @@
+def getConfig():
+  import json
+  config = json.loads(open("config.json").read())
+  return config
+
+
 def getHashtag():
   import json
   hashtag = input("Enter a hashtag to base predictions off: ").replace("#", "")
   return hashtag
+
 
 def getTweets():
   import json
@@ -9,11 +16,28 @@ def getTweets():
   hashtag = getHashtag()
   try:
     tweets = json.loads(open(hashtag + "Tweets.json").read())
-  except: #might want to check this is not an eror ^^
+  except: 
     print("No tweet database for this hashtag :/")
     sys.exit()
+  return tweets
 
-def continuousPredictor():
+    
+def makePrediction(text, tweets):
+  import re
+  config = getConfig()
+  predictions = {}
+  predictionDepth = config["predictionDepth"]
+  predictionLength = config["predictionLength"]
+  predictionBase = text.split()[-predictionDepth:]
+  for tweet in tweets:
+    if predictionBase in tweet:
+      indexes = [m.start() + len(predictionBase) for m in re.finditer(predictionBase, tweet)]
+      for index in index:
+        prediction = tweet[index:].split()[:2]
+     
+  
+def predictionLoop():
+  tweets = getTweets()
   print("Press control-C to escape loop")
-  while True:
+  while True:     
     pass
