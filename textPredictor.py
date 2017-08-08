6ca338dcf8a6bd6189a542f835c33588789a0354
Alex Pimania <randomiseme33@gmail.com>
2017-08-08 13:28:54 +1000

    Update textPredictor.py

--- textPredictor.py
+++ textPredictor.py
@@ -1,4 +1,19 @@
 def getHashtag():
+  import json
   hashtag = input("Enter a hashtag to base predictions off: ").replace("#", "")
+  return hashtag
+
+def getTweets():
+  import json
+  import sys
+  hashtag = getHashtag()
   try:
-    
+    tweets = json.loads(open(hashtag + "Tweets.json").read())
+  except: #might want to check this is not an eror ^^
+    print("No tweet database for this hashtag :/")
+    sys.exit()
+
+def continuousPredictor():
+  print("Press control-C to escape loop")
+  while True:
+    pass
